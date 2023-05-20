2cc5e7ce5f29cd6d63fbcea39e7565b03ad1476a
Alex Liu <dsa1314@gmail.com>
2023-05-21 03:37:42 +0800

    feat: rewrite to version 2 (#1)
    * feat: initial v2
    
    * build: update vercel.json
    
    * build: update vercel.json
    
    * build: update vercel.json
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * chore: update package.json
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * build: update vercel setting
    
    * test
    
    * test
    
    * test
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * test
    
    * test
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,86 @@
-import http from 'http'
-import createImage from './app'
+import { createServer } from 'node:http'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createApp, createRouter, eventHandler, getQuery, getRouterParams, toNodeListener } from 'h3'
+import {
+  GlobalFonts,
+  createCanvas,
+  loadImage,
+} from '@napi-rs/canvas'
 
-const PORT = 3300
-const server = http
-  .createServer((req, res) => {
-    if (req.url === '/favicon.ico') {
-      // control for favicon
-      res.writeHead(204, { 'Content-Type': 'image/x-icon' })
-      res.end()
-      return
-    }
-    createImage(req, res)
-  })
-  .listen(PORT, () => {
-    console.log(`success listen as http://localhost:${PORT}`)
-  })
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+GlobalFonts.registerFromPath(resolve(__dirname, './assets/fonts/Noto_Sans_TC/NotoSansTC-Bold.otf'))
+GlobalFonts.registerFromPath(resolve(__dirname, './assets/fonts/Inter/Inter-Bold.ttf'))
+
+const ONE_YEAR = 60 * 60 * 24 * 365
+
+function format(date: number | Date) {
+  return new Intl.DateTimeFormat('en-US', { month: 'short', day: 'numeric', year: 'numeric' }).format(date)
+}
+
+const app = createApp()
+const router = createRouter()
+
+router.get('/favicon.ico', eventHandler(({ node }) => {
+  node.res.statusCode = 204
+  node.res.setHeader('Content-Type', 'image/x-icon')
+}))
+
+const cache = new Map<string, Buffer>()
+
+router.get('/:title', eventHandler(async(event) => {
+  const { res } = event.node
+
+  const { title: _title } = getRouterParams(event)
+  let { create, read } = getQuery(event) as Record<string, string | string[]>
+
+  const title = decodeURIComponent(_title).trim().split(/(.{0,32})(?:\s|$)/g).filter(Boolean)
+
+  create = Array.isArray(create) ? create[0] : create
+  create = format(Number(create))
+
+  read = Array.isArray(read) ? read[0] : read
+
+  const meta = `${create} • ${read}`
+
+  const key = JSON.stringify({ title, meta })
+
+  if (!cache.has(key)) {
+    const canvas = createCanvas(1200, 630)
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = '#121212'
+    ctx.fillRect(0, 0, 1200, 630)
+    ctx.fillStyle = ctx.createPattern(await loadImage(resolve(__dirname, './assets/template.png')), 'repeat')
+    ctx.fillRect(0, 0, 1200, 630)
+
+    ctx.font = "bold 48px Inter, 'Noto Sans TC'"
+    ctx.fillStyle = '#F3F3F3'
+    title.forEach((text, index) => ctx.fillText(text, 73, 255 + index * 70))
+
+    ctx.font = "bold 24px Inter, 'Noto Sans TC'"
+    ctx.fillStyle = '#838383'
+    ctx.fillText(meta, 73, 300 + (title.length - 1) * 70)
+
+    cache.set(key, await canvas.encode('png'))
+  }
+
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'image/png')
+  res.setHeader(
+    'Cache-Control',
+    `public, immutable, no-transform, s-maxage=${ONE_YEAR}, max-age=${ONE_YEAR}`,
+  )
+
+  res.end(cache.get(key))
+}))
+
+app.use(router)
+
+createServer(toNodeListener(app)).listen(process.env.PORT || 3300)
+
+process.on('SIGINT', () => {
+  process.exit(0)
+})
